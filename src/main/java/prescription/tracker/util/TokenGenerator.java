c0ab3514b991c16d8340353c469fcc80d44af40a
Jose M <josem.marinhernandez@gmail.com>
2023-10-02 16:22:25 -0400

    Add functionality to generate random tokens with expiration date

--- src/main/java/prescription/tracker/util/TokenGenerator.java
+++ src/main/java/prescription/tracker/util/TokenGenerator.java
@@ -1,5 +1,18 @@
 package prescription.tracker.util;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class TokenGenerator {
+	
+	private static final int TOKEN_EXPIRATION_HOUR = 24;
+	
+	public static Token generateToken() {
+		
+		Token token = new Token();
+		token.setValue(UUID.randomUUID().toString());
+		token.setExpirationDateTime(LocalDateTime.now().plusHours(TOKEN_EXPIRATION_HOUR));
+		return token;
+	}
 
 }
