ae776c24846dab29dc2b5d9c2243cfe321c0fdf1
Jose M <josem.marinhernandez@gmail.com>
2023-10-03 00:49:17 -0400

    Remove /target/ from tracking

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"prescription.tracker"},{"l":"prescription.tracker.configuration"},{"l":"prescription.tracker.exception"},{"l":"prescription.tracker.medication"},{"l":"prescription.tracker.user"}];updateSearchResults();
