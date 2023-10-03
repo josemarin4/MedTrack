fb4ab43b47fe77c4c337e168d79a35aef11b11c3
Jose M <josem.marinhernandez@gmail.com>
2023-10-03 00:53:26 -0400

    Implement constructor for email service class

--- src/main/java/prescription/tracker/email/EmailService.java
+++ src/main/java/prescription/tracker/email/EmailService.java
@@ -1,5 +1,18 @@
 package prescription.tracker.email;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+@Service
 public class EmailService {
+	
+	private JavaMailSender javaMailSender;
+	private TemplateEngine templateEngine;
+	
+	public EmailService(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
+		this.javaMailSender = javaMailSender;
+		this.templateEngine = templateEngine;
+	}
 
 }
