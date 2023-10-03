e83f7737bbe6b0dfd34bfd9396f3b5227e5291d6
Jose M <josem.marinhernandez@gmail.com>
2023-10-03 02:32:21 -0400

    Implement mail sender for confirmation email

--- src/main/java/prescription/tracker/email/EmailService.java
+++ src/main/java/prescription/tracker/email/EmailService.java
@@ -1,18 +1,48 @@
 package prescription.tracker.email;
 
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.thymeleaf.context.Context;
+
 @Service
 public class EmailService {
-	
+
 	private JavaMailSender javaMailSender;
 	private TemplateEngine templateEngine;
-	
+
 	public EmailService(JavaMailSender javaMailSender, TemplateEngine templateEngine) {
 		this.javaMailSender = javaMailSender;
 		this.templateEngine = templateEngine;
+	}
+
+	public void sendConfirmationEmail(String recipientEmail, String confirmationToken){
+
+		try {
+			
+			MimeMessage message = javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			
+			helper.addTo(recipientEmail);
+			helper.setSubject("Confirm Your Email");
+			
+			Context context = new Context();
+			context.setVariable("confirmationLink", "/confirm?token=" + confirmationToken);
+			String emailContent = templateEngine.process("confirmationEmailTemplate", context);
+			
+			helper.setText(emailContent, true);
+			
+			javaMailSender.send(message);
+			
+		}
+		catch(MessagingException ex) {
+			ex.printStackTrace();
+
+		}
 	}
 
 }
