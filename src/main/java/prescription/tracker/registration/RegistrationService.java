459aa8edf882045a597a81930020ce47f783d876
Jose M <josem.marinhernandez@gmail.com>
2023-10-03 23:47:20 -0400

    Implements register user logic

--- src/main/java/prescription/tracker/registration/RegistrationService.java
+++ src/main/java/prescription/tracker/registration/RegistrationService.java
@@ -1,5 +1,43 @@
 package prescription.tracker.registration;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import prescription.tracker.exception.DuplicateUserException;
+import prescription.tracker.user.User;
+import prescription.tracker.util.Token;
+import prescription.tracker.util.TokenGenerator;
+
+@Service
 public class RegistrationService {
+	
+	private RegistrationRepository registrationRepository;
+	private PasswordEncoder passwordEncoder;
+	
+	public RegistrationService(RegistrationRepository registrationRepository, 
+			PasswordEncoder passwordEncoder) {
+		this.registrationRepository = registrationRepository;
+		this.passwordEncoder = passwordEncoder;
+		
+	}
+	/**
+	 * Registers a new user.
+	 * @param user The user to register.
+	 * @throws DuplicateUserException if the email is already taken.
+	 */
+	public void register(User user) {
+		registrationRepository.findUserByEmail(user.getEmail()).orElseThrow(() -> {
+			return new DuplicateUserException("Username " + user.getEmail() + " already taken.");
+		});
+		
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		
+		Token token = TokenGenerator.generateToken();
+		user.setConfirmationToken(token.getValue());
+		user.setConfirmationTokenExpiration(token.getExpirationDateTime());
+		
+		registrationRepository.save(user);
+	}
 
 }
