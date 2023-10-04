29ec50c2679ac79984a1e42023a6db033c86c820
Jose M <josem.marinhernandez@gmail.com>
2023-10-04 00:01:12 -0400

    Create a registration controller

---
+++ src/main/java/prescription/tracker/registration/RegistrationaController.java
@@ -0,0 +1,8 @@
+package prescription.tracker.registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/register")
+public class RegistrationaController {
+
+}
