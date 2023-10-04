1586a44b04d06180d097f0c5bc22ca3fe6e3a638
Jose M <josem.marinhernandez@gmail.com>
2023-10-03 23:52:07 -0400

    Remove register endpoint from user controller

--- src/main/java/prescription/tracker/user/UserController.java
+++ src/main/java/prescription/tracker/user/UserController.java
@@ -1,11 +1,9 @@
 package prescription.tracker.user;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +17,6 @@ public class UserController {
 		this.userService = userService;
 	}
 	
-	
-	@PostMapping("/register")
-	public ResponseEntity<User> register(@RequestBody User user){
-		
-		userService.register(user);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(user);
-		
-	}
 	@GetMapping("/{userId}")
 	public ResponseEntity<User> getUser(@PathVariable Long userId) {
 		
