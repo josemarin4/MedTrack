3cb52d6a48da29555618be3b943ad419d733742c
Jose M <josem.marinhernandez@gmail.com>
2023-10-04 00:04:19 -0400

    Add post endpoint to register

--- src/main/java/prescription/tracker/registration/RegistrationaController.java
+++ src/main/java/prescription/tracker/registration/RegistrationaController.java
@@ -1,8 +1,20 @@
 package prescription.tracker.registration;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import prescription.tracker.user.User;
 
 @RequestMapping("/api/register")
 public class RegistrationaController {
+	
+	@PostMapping()
+	public ResponseEntity<String> register(@RequestBody User user){
+		
+		return ResponseEntity.ok("Reigstration Successfull");
+		
+	}
 
 }
