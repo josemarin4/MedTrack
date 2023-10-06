dd32de507c6685714f5baea6d8543e99367e17c7
Jose M <josem.marinhernandez@gmail.com>
2023-10-06 13:38:31 -0400

    Add post mapping endpoint to register user

--- src/main/java/prescription/tracker/registration/RegistrationaController.java
+++ src/main/java/prescription/tracker/registration/RegistrationaController.java
@@ -4,16 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import prescription.tracker.user.User;
 
+@RestController
 @RequestMapping("/api/register")
 public class RegistrationaController {
 	
+	private RegistrationService registrationService;
+	
+	
+	public RegistrationaController(RegistrationService registrationService) {
+		this.registrationService = registrationService;
+	}
 	@PostMapping()
-	public ResponseEntity<String> register(@RequestBody User user){
-		
-		return ResponseEntity.ok("Reigstration Successfull");
+	public ResponseEntity<User> register(@RequestBody User user){
+		return ResponseEntity.ok(registrationService.register(user));
 		
 	}
 
