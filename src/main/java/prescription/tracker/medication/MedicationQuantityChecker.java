6af0738c154a54c007dd700eb93f3c4f9bd2ef71
Jose M <josem.marinhernandez@gmail.com>
2023-10-07 18:20:11 -0400

    Add function to compute quantity left of medications

--- src/main/java/prescription/tracker/medication/MedicationQuantityChecker.java
+++ src/main/java/prescription/tracker/medication/MedicationQuantityChecker.java
@@ -1,5 +1,22 @@
 package prescription.tracker.medication;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class MedicationQuantityChecker {
+	
+	private MedicationService medicationService;
+	
+	
+	
+	
+	public long quantityLef(Medication medication) {
+		long daysBetween = ChronoUnit.DAYS.between(medication.getLastRefilled(), LocalDate.now());
+		 return medication.getQuantity() - (medication.getTimesPerDay() * daysBetween);
+
+	}
 
 }
