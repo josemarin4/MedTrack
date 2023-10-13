5362f14d2b732dd0b9c5ea7421ec0687d406ddd1
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-10-13 17:17:50 -0400

    Add test for default constructor

--- src/test/java/prescripcion/tracker/test/user/UserTest.java
+++ src/test/java/prescripcion/tracker/test/user/UserTest.java
@@ -1,5 +1,23 @@
 package prescripcion.tracker.test.user;
 
+import prescription.tracker.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserTest {
+
+	@Test
+	public void testDefaultConstructor() {
+		User user = new User();
+		
+		assertNull(user.getEmail());
+		assertNull(user.getPassword());
+		assertNull(user.getConfirmationToken());
+		assertNull(user.getConfirmationTokenExpiration());
+		assertNull(user.getIsEnabled());
+		assertNull(user.getMedications());
+
+	}
 
 }
