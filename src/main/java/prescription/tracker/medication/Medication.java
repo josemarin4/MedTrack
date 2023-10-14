3ecf89e480eefce9186b6be15036c707b1ec6116
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-10-14 00:53:35 -0400

    Refactor datatype to primitive to prevent null values

--- src/main/java/prescription/tracker/medication/Medication.java
+++ src/main/java/prescription/tracker/medication/Medication.java
@@ -1,6 +1,8 @@
 package prescription.tracker.medication;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +22,6 @@ import prescription.tracker.user.User;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Medication {
 	
@@ -28,15 +29,16 @@ public class Medication {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long medId;
 	private String  name;
-	private Double dosage;
-	private Integer quantity;
-	private Integer refills;
-	private Integer timesPerDay;
+	private double dosage;
+	private int quantity;
+	private int refills;
+	private int timesPerDay;
 	private LocalDate lastRefilled;
 	private int reminderDays;
 	private LocalDate reminderDate;
 	
 	private static final int DEFAULT_REMINDER_DAYS = 7;
+	private static final int DEFAULT_QUANTITY = 0;
 	
 	//Join column annotation specifies the foreign key column (user_id) 
 	//in the medication table that references the user table.
@@ -44,21 +46,46 @@ public class Medication {
 	@JoinColumn(name = "userId")
 	private User user;
 	
+	public Medication(Long medId, String name, Double dosage, Integer quantity, Integer refills, Integer timesPerDay,
+			LocalDate lastRefilled, int reminderDays, User user) {
+		this.medId = medId;
+		this.name = name;
+		setDosage(dosage);
+		this.quantity = quantity;
+		setRefills(refills);
+		setTimesPerDay(timesPerDay);
+		this.lastRefilled = lastRefilled;
+		setReminderDays(reminderDays);
+		calculateReminderDate();
+		this.user = user;
+	}
 	
 	public void setTimesPerDay(Integer timesPerDay) {
-		//ensures remaining quantity is non-negative
-		this.timesPerDay = timesPerDay < 0? 0 : timesPerDay;
+		if(timesPerDay == null || timesPerDay < 0) {
+			this.timesPerDay = DEFAULT_QUANTITY;
+		}
+		else {
+			this.timesPerDay = timesPerDay;
+		}
 	}
 	
 	public void setDosage(Double dosage) {
 		
-		//ensures dosage is non-negative
-		this.dosage = dosage < 0? 0 : dosage;
+		if(dosage == null || dosage < 0) {
+			this.dosage = (double) DEFAULT_QUANTITY;
+		}
+		else {
+			this.dosage = dosage;
+		}
 	}
 	
 	public void setRefills(Integer refills) {
-		//ensures refills is non-negative
-		this.refills = refills < 0? 0 : refills;
+		if(refills == null || refills < 0) {
+			this.refills = DEFAULT_QUANTITY;
+		}
+		else {
+			this.refills = refills;
+		}
 	}
 	
 	public void setReminderDays(int reminderDays) {
@@ -70,5 +97,15 @@ public class Medication {
 			this.reminderDays = reminderDays;
 		}
 	}
+	
+	private void calculateReminderDate() {
+		
+		long ammonutToRemind = timesPerDay * reminderDays;
+		long pillsLeft = quantity - ammonutToRemind;
+		long days = pillsLeft / timesPerDay;
+		reminderDate = ChronoUnit.DAYS.addTo(lastRefilled, days);
+		
+	}
+
 
 }
