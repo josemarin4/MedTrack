0ffefd7199632639903c191f3e7607323c272f06
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-10-14 22:44:30 -0400

    Add test for parametized constructor

--- src/test/java/prescripcion/tracker/test/medication/MedicationTest.java
+++ src/test/java/prescripcion/tracker/test/medication/MedicationTest.java
@@ -1,9 +1,12 @@
 package prescripcion.tracker.test.medication;
 import prescription.tracker.medication.Medication;
+import prescription.tracker.user.User;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class MedicationTest {
 
@@ -12,5 +15,25 @@ public class MedicationTest {
 		Medication med = new Medication();
 		
 		assertEquals(LocalDate.now(), med.getLastRefilled());
+	}
+	
+	@Test
+	public void shouldCreateMedicationWithParameters() {
+		User user = new User(2L, "email", "password", true, new ArrayList<>());
+		Medication med = new Medication(1L, "name", 2.0, 30, 4, 2, LocalDate.now(), 6,
+			user);
+		
+		
+		assertEquals(1L, med.getMedId());
+		assertEquals("name", med.getName());
+		assertEquals(2.0, med.getDosage());
+		assertEquals(30, med.getQuantity());
+		assertEquals(4, med.getRefills());
+		assertEquals(2, med.getTimesPerDay());
+		assertEquals(LocalDate.now(), med.getLastRefilled());
+		assertEquals(6, med.getReminderDays());
+		assertEquals(user, med.getUser());
+		
+				
 	}
 }
