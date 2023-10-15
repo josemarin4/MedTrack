70b76c4309d4416304e32e36093b2dcb83a49fa5
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-10-14 22:22:43 -0400

    Implement test for empty constructor

--- src/test/java/prescripcion/tracker/test/medication/MedicationTest.java
+++ src/test/java/prescripcion/tracker/test/medication/MedicationTest.java
@@ -1,5 +1,16 @@
 package prescripcion.tracker.test.medication;
+import prescription.tracker.medication.Medication;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
 
 public class MedicationTest {
 
+	@Test
+	public void shouldCreateMedicationWithLastRefilledNow() {
+		Medication med = new Medication();
+		
+		assertEquals(LocalDate.now(), med.getLastRefilled());
+	}
 }
