50250603f3229c2c2c9d2d07a1dcee23657e33f6
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-10-20 00:16:47 -0400

    Add annotation to make sure email is unique and not null

--- src/main/java/prescription/tracker/user/User.java
+++ src/main/java/prescription/tracker/user/User.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,6 +34,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
 	
+	@Column(unique = true, nullable = false)
 	private String email;
 	private String password;
 	private String confirmationToken;
