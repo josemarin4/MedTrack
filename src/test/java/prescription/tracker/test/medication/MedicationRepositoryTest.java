8e3a3397bab9b4fbb3e27d7260d0bca9f5e8bfa3
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-11-14 22:03:42 -0500

    Implement test for deleting all medications from user

--- src/test/java/prescription/tracker/test/medication/MedicationRepositoryTest.java
+++ src/test/java/prescription/tracker/test/medication/MedicationRepositoryTest.java
@@ -1,5 +1,6 @@
 package prescription.tracker.test.medication;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -45,6 +46,34 @@ public class MedicationRepositoryTest {
 		
 		assertEquals(user.getMedications(), medications);
 				
+	}
+	
+	@Test
+	public void shouldDeleteAllMedicationsFromUserId() {
+		
+		Medication med1 = new Medication();
+		med1.setName("Med1");
+		Medication med2 = new Medication();
+		med2.setName("Med2");
+		
+		User user = new User();
+		user.setEmail("email@email.com");
+		user.setPassword("password");
+		user.setEnabled(true);
+		med1.setUser(user);
+		med2.setUser(user);
+		
+		entityManager.persistAndFlush(user);
+		entityManager.clear();
+		
+		medicationRepository.deleteAllByUserId(user.getUserId());
+		
+		List<Medication> medications = medicationRepository.findAllByUserId(user.getUserId()).orElse(null);
+		
+		assertNotNull(medications);
+		
+		assertEquals(Collections.EMPTY_LIST, medications);
+		
 	}
 
 }
