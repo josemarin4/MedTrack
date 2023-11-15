4d54cca5ddb56e715933c1c84dea096e5ab42327
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-11-14 22:04:29 -0500

    Add annotation to indicate modifying query

--- src/main/java/prescription/tracker/medication/MedicationRepository.java
+++ src/main/java/prescription/tracker/medication/MedicationRepository.java
@@ -3,6 +3,7 @@ package prescription.tracker.medication;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 /**
@@ -31,6 +32,7 @@ public interface MedicationRepository extends JpaRepository<Medication, Long> {
 	 * 
 	 * @param userId The unique identifier of the user whose medications are to be deleted.
 	 */
+	@Modifying
 	@Query("DELETE FROM Medication m WHERE m.user.userId =:userId")
 	void deleteAllByUserId(Long userId);
 }
