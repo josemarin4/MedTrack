2d6eaf3d46bbafd2b40ae66f7a3f7090103df03f
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2023-11-18 16:10:04 -0500

    Add test case for getting user using given id

--- src/test/java/prescription/tracker/test/user/UserControllerTest.java
+++ src/test/java/prescription/tracker/test/user/UserControllerTest.java
@@ -1,5 +1,47 @@
 package prescription.tracker.test.user;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import prescription.tracker.user.User;
+import prescription.tracker.user.UserController;
+import prescription.tracker.user.UserService;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private UserService userService;
+	
+	@Test
+	public void shouldGetUser() throws Exception{
+		
+		User user = new User(2L, "email@email.com", "password", true, Collections.emptyList());
+		
+		given(userService.getUser(2L))
+			.willReturn(user);
+		
+		mockMvc.perform(get("/api/user/{userId}", 2L))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(content().json(new ObjectMapper().writeValueAsString(user)));
+			
+	}
 
 }
